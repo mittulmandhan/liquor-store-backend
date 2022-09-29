d3a203e3b20e7f3f3205d56ab715b49418f84bd4
mittulmandhan <conanmittul@gmail.com>
2022-09-29 16:52:23 +0530

    department service initialized

---
+++ department-service/src/test/java/com/liqour/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liqour.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

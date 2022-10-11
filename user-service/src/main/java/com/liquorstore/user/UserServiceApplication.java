d07e2cfab9e6f9633213598b2225ff8a9666228a
mittulmandhan <conanmittul@gmail.com>
2022-10-11 21:20:48 +0530

    service registry using eureka

--- user-service/src/main/java/com/liquorstore/user/UserServiceApplication.java
+++ user-service/src/main/java/com/liquorstore/user/UserServiceApplication.java
@@ -3,6 +3,7 @@ package com.liquorstore.user;
 import com.liquorstore.user.VO.ResponseTemplateVO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,6 +15,7 @@ public class UserServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
--- user-service/src/main/java/com/liquorstore/user/server_impl/UserServiceImpl.java
+++ user-service/src/main/java/com/liquorstore/user/server_impl/UserServiceImpl.java
@@ -31,7 +31,7 @@ public class UserServiceImpl implements UserService {
         CustomUser user = userRepository.findByUserId(userId);
 
         Department department =
-                restTemplate.getForObject("http://localhost:9001/departments/" + user.getDepartmentId()
+                restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/" + user.getDepartmentId()
                         , Department.class);
         vo.setUser(user);
         vo.setDepartment(department);

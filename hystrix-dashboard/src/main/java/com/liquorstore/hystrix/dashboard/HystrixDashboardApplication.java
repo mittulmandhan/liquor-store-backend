632f46a5d053bfd2f309ceb4fa40aa3228c2f13f
mittulmandhan <conanmittul@gmail.com>
2022-10-12 04:50:10 +0530

    hystrix dashboard service added

---
+++ hystrix-dashboard/src/main/java/com/liquorstore/hystrix/dashboard/HystrixDashboardApplication.java
@@ -0,0 +1,14 @@
+package com.liquorstore.hystrix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+
+}
